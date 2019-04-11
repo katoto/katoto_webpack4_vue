6959e64ec7a1ecb49e8b88a43bcf819b76ad9c8f
guowf <380482989@qq.com>
2019-04-11 18:01:29 +0800

    ''

--- src/pages/mall/components/pop/Pop_real2card.vue
+++ src/pages/mall/components/pop/Pop_real2card.vue
@@ -1,49 +1,48 @@
 <template>
-    <Pop class="pop_rechange_many" :show.sync="show">
-        <div class="h3 pop_name">{{_('m_payment.exchange_success')}}</div>
-        <img class="product_img" src="@assets/img/amazon.png" alt>
-        <div class="product_msg">
-            <p class="product_name">100 Amazon</p>
-            <p class="product_count">({{_('m_payment.virtual_num', 3)}})</p>
-        </div>
-        <p class="product_use">1000 gift card for Amazon Mail</p>
-        <div class="pop_rechange_many_main ">
-            <ul>
-                <li class="pop_rechange_many_list">
-                    <p>
-                        <span class="card_num">{{_('m_payment.card_no')}}:</span>
-                        <i class="card_psw">12{{ list }}56484359765815</i>
-                    </p>
-                    <p>
-                        <span class="card_num">{{_('m_payment.password')}}:</span>
-                        <i class="card_psw">654235654235654235654</i>
-                    </p>
-                </li>
-                <li class="pop_rechange_many_list">
-                    <p>
-                        <span class="card_num">{{_('m_payment.card_no')}}:</span>
-                        <i class="card_psw">1256484359765815</i>
-                    </p>
-                    <p>
-                        <span class="card_num">{{_('m_payment.password')}}:</span>
-                        <i class="card_psw">654235654235654235654235654235654235</i>
-                    </p>
-                </li>
-                <li class="pop_rechange_many_list">
-                    <p>
-                        <span class="card_num">{{_('m_payment.card_no')}}:</span>
-                        <i class="card_psw">{{ list }}12{{ list }}56484359765815</i>
-                    </p>
-                    <p>
-                        <span class="card_num">{{_('m_payment.password')}}:</span>
-                        <i class="card_psw">65423565423565{}4235654235654235654235</i>
-                    </p>
-                </li>
-            </ul>
-        </div>
-        <a href="javascript:" class="btn_default">{{_('m_payment.copy_all')}}</a>
-    </Pop>
-
+  <Pop class="pop_rechange_many" :show.sync="show">
+    <div class="h3 pop_name">{{_('m_payment.exchange_success')}}</div>
+    <img class="product_img" src="@assets/img/amazon.png" alt>
+    <div class="product_msg">
+      <p class="product_name"></p>
+      <p class="product_count">({{_('m_payment.virtual_num', 3)}})</p>
+    </div>
+    <p class="product_use">1000 gift card for Amazon Mail</p>
+    <div class="pop_rechange_many_main">
+      <ul>
+        <li class="pop_rechange_many_list">
+          <p>
+            <span class="card_num">{{_('m_payment.card_no')}}:</span>
+            <i class="card_psw"></i>
+          </p>
+          <p>
+            <span class="card_num">{{_('m_payment.password')}}:</span>
+            <i class="card_psw">654235654235654235654</i>
+          </p>
+        </li>
+        <li class="pop_rechange_many_list">
+          <p>
+            <span class="card_num">{{_('m_payment.card_no')}}:</span>
+            <i class="card_psw">1256484359765815</i>
+          </p>
+          <p>
+            <span class="card_num">{{_('m_payment.password')}}:</span>
+            <i class="card_psw">654235654235654235654235654235654235</i>
+          </p>
+        </li>
+        <li class="pop_rechange_many_list">
+          <p>
+            <span class="card_num">{{_('m_payment.card_no')}}:</span>
+            <i class="card_psw"></i>
+          </p>
+          <p>
+            <span class="card_num">{{_('m_payment.password')}}:</span>
+            <i class="card_psw">65423565423565{}4235654235654235654235</i>
+          </p>
+        </li>
+      </ul>
+    </div>
+    <a href="javascript:" class="btn_default">{{_('m_payment.copy_all')}}</a>
+  </Pop>
 </template>
 <script>
 import Pop from "./Pop.vue"
@@ -64,6 +63,7 @@ export default {
         },
         list:{
             get:function(){
+                console.log(this.app.record_currlist)
                 return this.app.record_currlist
             }
         }
