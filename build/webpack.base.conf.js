f535629d8e2e717db9432dd4477a85f170a9cdf0
yudf(余大富) <yudf@500wan.com>
2019-04-16 10:11:54 +0800

    更新

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -59,7 +59,7 @@ module.exports = {
                 use:[{
                     loader: 'url-loader',
                     options: {
-                        limit: 10000,
+                        limit: 20000,
                         name: utils.assetsPath('img/[name].[ext]')
                     }
                 },{
