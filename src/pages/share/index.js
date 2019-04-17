ef3d21111f6faedc5d13a44be111597cb319f9ce
coinslot <xiezg@qq.com>
2019-04-17 10:41:27 +0800

    417

--- src/pages/share/index.js
+++ src/pages/share/index.js
@@ -7,7 +7,8 @@ import simpleAjax from "@plugins/simpleAjax"
 import Toast from "@/components/toast/toast.js"
 
 import {
-    cookie
+    cookie,
+    getURLParams
 } from "@common/util"
 
 Vue.prototype.$toast = Toast
@@ -19,16 +20,19 @@ require("@/styles/lib-public.less")
 
 // test  注入的是www.madcrickets.com/
 // cookie.set("appck", encodeURIComponent("MTAwMDI3NTI5N2I2MWIyMGIwOTQ1YjRmMTUzYTcwMDZmZTE1YjEy") )
-// cookie.set("language", "hi" )
+cookie.set("language", "en" )
 
 let language = cookie.get("language") || "en"
-let ck = cookie.get("appck") || "en"
 
+let ck = cookie.get("appck")
+if (!ck || ck === "undefined") {
+    ck = getURLParams().appck
+}
 // 引入语言包
 require("@language").use(Vue, [shareLanguage])
 if (!ck || ck === "undefined") {
     window.$toast({
-        content: "madsport open"
+        content: "please use madsport open this pages"
     })
 }
 
