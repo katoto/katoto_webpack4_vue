563ef8a9be6b311b76e32d8f8cab65bf96e88156
yudf(余大富) <yudf@500wan.com>
2019-04-18 10:45:08 +0800

    更新金额

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,16 +19,16 @@ module.exports = {
     ],
     // add your custom rules here
     "rules": {
-        //"off"或0 -关闭规则
-        //"warn" 或1 - 开启规则, 使用警告 程序不会退出
-        //"error"或2 - 开启规则, 使用错误 程序退出
-        "indent": ["error", 4],
+        //"off"或 0 -关闭规则
+        //"warn" 或 1 - 开启规则, 使用警告 程序不会退出
+        //"error"或 2 - 开启规则, 使用错误 程序退出
+        "indent": [2, 4],
         //分号
-        "eqeqeq": "off",
-        "no-useless-escape": "off",
+        "eqeqeq": 0,
+        "no-useless-escape": 0,
         "brace-style": 0,//大括号风格
-        "curly": ["error", "all"], //[2, "all"],//必须使用 if(){} 中的{}
-        "no-new": "off",
+        "curly": [2, "all"], //[2, "all"],//必须使用 if(){} 中的{}
+        "no-new": 0,
         "no-return-assign": "warn",//return 语句中不能有赋值表达式
 
         "handle-callback-err": 0,
@@ -40,35 +40,36 @@ module.exports = {
 
         "no-debugger": 0,
         "no-eval": 0,
-        "comma-dangle": ["error", "never"],
-        "arrow-spacing": ["error", { "before": true, "after": true }],
-        "no-undef": "error",
+        "comma-dangle": [2, "never"],
+        "arrow-spacing": [2, { "before": true, "after": true }],
+        "no-undef": 2,
         "no-console": 0,
-        "space-before-function-paren": ["error", "always"],
-        "keyword-spacing": ["error", { "before": true, "after": true }],
-        "space-before-blocks": ["error", "always"],
-        "spaced-comment": ["error", "always", {"exceptions": ['-', '+']}],
-        "quotes": ["error", "double"],
-        "semi": ["error", "never"],
-        "no-multiple-empty-lines": ["error", {"max": 1}],
-        "generator-star-spacing": ["error", { "before": true, "after": true }],
-        "object-curly-newline": ["error", {
+        "space-before-function-paren": [2, "always"],
+        "keyword-spacing": [2, { "before": true, "after": true }],
+        "space-before-blocks": [2, "always"],
+        "spaced-comment": [2, "always", {"exceptions": ['-', '+']}],
+        "quotes": [2, "double"],
+        "semi": [2, "never"],
+        "no-multiple-empty-lines": [2, {"max": 1}],
+        "generator-star-spacing": [2, { "before": true, "after": true }],
+        "object-curly-newline": [2, {
             "ObjectPattern": {
                 "multiline": true
             }
         }],
-        "object-curly-spacing": ["error", "always"],
-        "key-spacing": ["error", { "beforeColon": false, "afterColon": true }],
-        "linebreak-style": ["error", "windows"],
-		"eol-last": ["error", "windows"],
-        "object-property-newline": ["error", {}],
-        "vue/html-indent": ["error", 4, {
+        "object-curly-spacing": [2, "always"],
+        "key-spacing": [2, { "beforeColon": false, "afterColon": true }],
+        "linebreak-style": [2, "windows"],
+		"eol-last": [2, "windows"],
+        "object-property-newline": [2, {}],
+        "vue/html-indent": [2, 4, {
             "attribute": 1,
             "baseIndent": 1,
             "closeBracket": 0,
             "alignAttributesVertically": true,
             "ignores": []
         }],
+        "no-control-regex": 0
         // "vue/html-closing-bracket-newline": ["error", {
         //     "multiline": "always"
         // }],
@@ -83,6 +84,7 @@ module.exports = {
     globals: {
         "_": true,
         "arguments": true,
-        "ActiveXObject": true
+        "ActiveXObject": true,
+        "AndroidLogin": true
     }
 }
