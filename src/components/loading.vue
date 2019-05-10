8e417ef59143614b37dfd3a1cefebedd4a0722eb
guowf <380482989@qq.com>
2019-05-10 15:25:27 +0800

    ''

---
+++ src/components/loading.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="loading">
+        <div class="common_loading"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+
+        }
+    },
+    components: {
+
+    },
+    computed: {},
+    methods: {},
+    mounted () {}
+}
+</script>
+
+<style lang="less" scoped type="text/less">
+@vw: 100/750vw;
+.loading {
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  z-index: 99;
+}
+</style>
